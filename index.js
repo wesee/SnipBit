487abfdf6467ce652e919aedfeec579b16629dc8
essofluffy <d.dehueck@yahoo.com>
2015-11-09 20:23:24 -0600

    Create index.js
    Main file to call

---
+++ index.js
@@ -0,0 +1,12 @@
+var server = require('./server');
+var router = require('./router');
+var requestHandlers = require('./requestHandlers');
+
+//Make an object containing request handlers for different paths
+var handle = {};
+handle['/'] = requestHandlers.hello;
+handle['/pay'] = requestHandlers.pay;
+handle['/ack'] = requestHandlers.ack;
+
+//Start server
+server.start(router.route, handle);
